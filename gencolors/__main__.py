1c1bcfc3b492f39f6031ef22b4aaefd8bd754838
Christopher Wells <cwellsny@nycap.rr.com>
2017-06-13 22:49:07 -0400

    Change to use numpy arrays
    Change the program to use numpy arrays in order to speed up the image
    writing.

--- gencolors/__main__.py
+++ gencolors/__main__.py
@@ -20,32 +20,16 @@ def main():
         population = genetics.next_generation(population)
 
 def get_average_score(population):
-    scores = [i.score() for i in population]
+    scores = [genetics.score_chromosome(i) for i in population]
 
     return np.mean(scores)
 
-# https://stackoverflow.com/questions/12062920/how-do-i-create-an-image-in-pil-using-a-list-of-rgb-tuples
 def create_population_image(population, img_name):
-    # Image size
-    width = int((2 ** (FACTOR / 2)) * SCALING)
-    height = int((2 ** (FACTOR / 2)) * SCALING)
-    channels = 3
-
-    # Create an empty image
-    img = np.zeros((height, width, channels), dtype=np.uint8)
-
-    # Set the RGB values
-    for y in range(img.shape[0]):
-        for x in range(img.shape[1]):
-            x_part = int(x / SCALING)
-            y_part = int(int(y / SCALING) * (width / SCALING))
-            index = x_part + y_part
-
-            i = population[index]
-            r, g, b = i.red, i.green, i.blue
-            img[y][x][0] = r
-            img[y][x][1] = g
-            img[y][x][2] = b
+    # Create the image from the population
+    dim = int(2 ** (FACTOR / 2))
+    img = np.reshape(population, (dim, dim, 3))
+    img = img.repeat(SCALING, axis=0) \
+             .repeat(SCALING, axis=1)
 
     # Save the image
     scipy.misc.imsave(img_name, img)
--- gencolors/genetics.py
+++ gencolors/genetics.py
@@ -1,82 +1,74 @@
 from random import randint
 
+import numpy as np
+
 GOAL = (52, 152, 219)
 MUTATION_CHANCE = 100
 MUTATION_RANGE = 15
 REPLACE_RATE = 1.1
 
-class Chromosome(object):
-    red = None
-    green = None
-    blue = None
+def score_chromosome(self):
+    r_err = abs(self[0] - GOAL[0])
+    g_err = abs(self[1] - GOAL[1])
+    b_err = abs(self[2] - GOAL[2])
 
-    def __init__(self, r, g,b):
-        self.red = r
-        self.green = g
-        self.blue = b
+    return r_err + g_err + b_err
 
-    def score(self):
-        r_err = abs(self.red - GOAL[0])
-        g_err = abs(self.green - GOAL[1])
-        b_err = abs(self.blue - GOAL[2])
+def mate_chromosome(self, other):
+    c_red = self[0]
+    c_green = other[1]
+    c_blue = self[2]
 
-        return r_err + g_err + b_err
+    child = np.array([c_red, c_green, c_blue])
 
-    def mate(self, other):
-        c_red = self.red
-        c_green = other.green
-        c_blue = self.blue
+    if randint(0, MUTATION_CHANCE) == 0:
+        r_change = randint(-MUTATION_RANGE, MUTATION_RANGE)
+        g_change = randint(-MUTATION_RANGE, MUTATION_RANGE)
+        b_change = randint(-MUTATION_RANGE, MUTATION_RANGE)
+        mutate_chromosome(child, r_change, g_change, b_change)
 
-        child = Chromosome(c_red, c_green, c_blue)
+    return child
 
-        if randint(0, MUTATION_CHANCE) == 0:
-            r_change = randint(-MUTATION_RANGE, MUTATION_RANGE)
-            g_change = randint(-MUTATION_RANGE, MUTATION_RANGE)
-            b_change = randint(-MUTATION_RANGE, MUTATION_RANGE)
-            child.mutate(r_change, g_change, b_change)
-
-        return child
-
-    def mutate(self, r_change, g_change, b_change):
-        self.red = self.red + r_change
-        self.green = self.green + g_change
-        self.blue = self.blue + b_change
-
+def mutate_chromosome(self, r_change, g_change, b_change):
+    self[0] = self[0] + r_change
+    self[1] = self[1] + g_change
+    self[2] = self[2] + b_change
 
 def create_population(num):
-    return [create_individual() for x in range(num)]
+    return np.array([create_individual() for x in range(num)])
 
 def create_individual():
     red = randint(0, 255)
     green = randint(0, 255)
     blue = randint(0, 255)
 
-    return Chromosome(red, green, blue)
+    return np.array([red, green, blue])
 
 def next_generation(population):
-    scores = [(x.score(), x) for x in population]
-    scores.sort(key=lambda tup: tup[0])
+    scores = np.array([score_chromosome(c) for c in population])
+    positions = np.argsort(scores)
+    sorted_population = population[positions]
 
-    midpoint = int(len(scores) / REPLACE_RATE)
-    survivors = [x for (_,x) in scores[:midpoint]]
+    midpoint = int(sorted_population.shape[0] / REPLACE_RATE)
+    survivors = sorted_population[:midpoint]
 
-    num_children = len(scores) - midpoint
+    num_children = sorted_population.shape[0] - survivors.shape[0]
     children = create_children(survivors, num_children)
 
-    new_population = survivors + children
+    new_population = np.concatenate([survivors, children])
     return new_population
 
 def create_children(survivors, num_children):
-    n_survivors = len(survivors)
+    n_survivors = survivors.shape[0]
     parents = [(randint(0, n_survivors - 1), randint(0, n_survivors - 1)) for _ in range(num_children)]
     
     children = [mate(survivors, par) for par in parents]
 
-    return children
+    return np.array(children)
 
 def mate(survivors, parents):
     p_1 = survivors[parents[0]]
     p_2 = survivors[parents[1]]
-    child = p_1.mate(p_2)
+    child = mate_chromosome(p_1, p_2)
 
     return child
